c7fbf56bd584aa09084e3ce49b4794e28ac654a6
Victoria Rathsmill <vlrathsmill@gmail.com>
2017-01-17 20:07:47 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+
+  data.each do |info, info_hash|
+    info_hash.each do |description, names_array|
+      names_array.each do |name|
+        if !pigeon_list.has_key?(name)
+          pigeon_list[name] = {}
+        end
+
+      if !pigeon_list[name].has_key?(info)
+        pigeon_list[name][info] = []
+      end
+
+      if !pigeon_list[name][info].include?(description)
+        pigeon_list[name][info] << description.to_s
+      end
+    end
+  end
+end
+  pigeon_list
 end
